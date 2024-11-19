15f0a9d24af9442ccd8c2be6cc82ad1651695630
Leonardo Gabriel <126363100+LeonardooGabriel@users.noreply.github.com>
2024-11-19 00:22:14 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var titulo = document.querySelector('h1');
+titulo.textContent = "Leonardo Nutricionista";
+
+var titulo = document.querySelector('h2');
+titulo.textContent = "Pacientes Cadastros";
+
+var paciente = document.querySelector('#Primeiro-Paciente');
+console.log(paciente);
